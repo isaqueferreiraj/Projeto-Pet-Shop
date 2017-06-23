d7525a612e74e4e361284072d752297806186607
isaqueferreiraj <isaqueferreiraj@gmail.com>
2017-06-23 17:34:58 -0300

    Cadastro Produtos 1.1

--- ProjetoPetShop/src/pet_shop/negocio/CadastroProdutos.java
+++ ProjetoPetShop/src/pet_shop/negocio/CadastroProdutos.java
@@ -11,6 +11,7 @@ public class CadastroProdutos {
 	    this.repositorioProdutos = instanciaRepositorio;
 	}
 	
+	//Cadastro do produto
 	public void cadastrar(Produto p) {
 	    if (p == null) {
 	      //Exceção
@@ -23,6 +24,7 @@ public class CadastroProdutos {
 	    }
 	}
 	
+	//Descadastra o produto do id repassado, depois de verificar a existência do produto
 	public void descadastrar(long id){
 		Produto p = this.repositorioProdutos.listarProduto(id);
 		if(p != null){
@@ -32,14 +34,17 @@ public class CadastroProdutos {
 			System.out.println("Aqui será colocada a exceção");
 	}
 	
+	//Lista produto de acordo com o id repassado
 	public Produto listar(long id){
 		return this.repositorioProdutos.listarProduto(id);
 	}
 	
+	//Verifica se o produto existe
 	public boolean existe(Produto p) {
 	    return this.repositorioProdutos.existe(p);
 	}
 	
+	//Exclui o produto de acordo com o id repassado
 	public void excluirProduto(long id){
 		this.repositorioProdutos.excluirProduto(id);
 	}
